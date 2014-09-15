5ad94ea78274f8326cf08e36b11495780a4c3522
Sayantan <sayghosh@ebay.com>
2014-09-15 01:34:25 -0700

    1. Probability of getting more than n clicks per day if the ad clicks are a normal distribution

--- StatisticalInference.R
+++ StatisticalInference.R
@@ -1,5 +1,11 @@
 
 ###Common Distributions
+#What is the probability of getting more than 1160 ad clicks in a day, if the ad clicks are normally distributed.
+#Mean Ad clicks = 1020, sd of ad clicks = 50
+pnorm(1160, mean=1020, sd =50, lower.tail=FALSE);
+#1160 is 2.8 standard deviations from the mean
+pnorm(2.8, lower.tail=FALSE);
+
 #What number of ad clicks would represent the one where 75% of the days have fewer ad clicks
 #Mean Ad clicks = 1020, sd of ad clicks = 50
 qnorm(.75, mean=1020, sd=50);
