091e025fb0714588bc53158ea5803cda10803cd9
Sayantan <sayghosh@ebay.com>
2014-09-15 20:56:21 -0700

    1.Overlayed density plots 2. Box plots  TODO : scatter plots and regression line

--- caret_tutorial.R
+++ caret_tutorial.R
@@ -2,7 +2,7 @@
 ## 1. Visualizations
 #####################################
 
-#Scatter Plot Matrix
+# 1. Scatter Plot Matrix
 library(datasets);
 data(iris);
 library(AppliedPredictiveModeling);
@@ -15,3 +15,26 @@ featurePlot(x = iris[, 1:4],
             ## Add a key at the top #sentosa, versicolor, virginica
             auto.key = list(columns = 3));
 
+# 2. Overlayed density plots
+transparentTheme(trans = .9);
+featurePlot(x = iris[, 1:4],
+            y = iris$Species,
+            plot = "density",
+            ## Pass in options to xyplot() to 
+            ## make it prettier
+            scales = list(x = list(relation="free"),
+                          y = list(relation="free")),
+            adjust = 1.5,
+            pch = "|",
+            layout = c(4, 1),
+            auto.key = list(columns = 3));
+
+# 3. Box plots
+featurePlot(x = iris[, 1:4],
+            y = iris$Species,
+            plot = "box",
+            ## Pass in options to bwplot() 
+            scales = list(y = list(relation="free"),
+                          x = list(rot = 90)),
+            layout = c(4,1 ),
+            auto.key = list(columns = 3));
