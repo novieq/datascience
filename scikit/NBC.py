91a25fcc7f37d196e669803f658096d3443dfa38
Sayantan <sayghosh@ebay.com>
2014-09-24 00:40:49 -0700

    Adding multinomial and bernoulli NBC. Saving the classifier to the disk and loading it

--- scikit/NBC.py
+++ scikit/NBC.py
@@ -1,7 +1,26 @@
 from sklearn import datasets
 iris = datasets.load_iris()
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 gnb = GaussianNB()
 y_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
 print("Number of mislabeled points out of a total %d points : %d" % (iris.data.shape[0],(iris.target != y_pred).sum()))
-#Number of mislabeled points out of a total 150 points : 6
+
+mnb = MultinomialNB()
+y_pred_mnb = mnb.fit(iris.data, iris.target).predict(iris.data)
+print("Number of mislabeled points out of a total %d points : %d" % (iris.data.shape[0],(iris.target != y_pred_mnb).sum()))
+
+bnb = BernoulliNB()
+y_pred_bnb = bnb.fit(iris.data, iris.target).predict(iris.data)
+print("Number of mislabeled points out of a total %d points : %d" % (iris.data.shape[0],(iris.target != y_pred_bnb).sum()))
+
+import cPickle
+# save the classifier
+with open('my_dumped_classifier.pkl', 'wb') as fid:
+    cPickle.dump(gnb, fid)    
+
+# load it again
+with open('my_dumped_classifier.pkl', 'rb') as fid:
+    gnb_loaded = cPickle.load(fid)
+
+y_pred_loaded = gnb_loaded.fit(iris.data, iris.target).predict(iris.data)
+print("Number of mislabeled points out of a total %d points : %d" % (iris.data.shape[0],(iris.target != y_pred_loaded).sum()))
